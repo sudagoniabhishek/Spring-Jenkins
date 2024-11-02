b433896118b850dbe09a648971f313aff1659950
Sudagoni Abhishek <sudagonia@italentdigital.com>
2024-11-02 13:42:43 +0530

    Third commit

--- src/test/java/com/Spring/Jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/Spring/Jenkins/SpringJenkinsApplicationTests.java
@@ -15,6 +15,7 @@ class SpringJenkinsApplicationTests {
 	void contextLoads() {
 		logger.info("Test case executing...");
 		logger.info("Test case executing second log statement...");
+		logger.info("Test case executing third log statement...");
 		assertEquals(true, true);
 	}
 
