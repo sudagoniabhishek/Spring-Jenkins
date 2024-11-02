c241150d5cd04cb195a48d25b482e6c9224b54d5
Sudagoni Abhishek <sudagonia@italentdigital.com>
2024-11-02 22:41:44 +0530

    removing logger commit

--- src/test/java/com/Spring/Jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/Spring/Jenkins/SpringJenkinsApplicationTests.java
@@ -14,8 +14,6 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing...");
-		logger.info("Test case executing second log statement...");
-		logger.info("Test case executing third log statement...");
 		assertEquals(true, true);
 	}
 
